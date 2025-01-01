be97cf6015a19de398788acb4da5a09a83afc5bb
Sophia See <sophiasee11@gmail.com>
2025-01-01 22:09:57 +0800

    added transition in technology

--- src/components/Technology/Technology.tsx
+++ src/components/Technology/Technology.tsx
@@ -2,19 +2,30 @@ import { useNavigate, useParams } from 'react-router-dom';
 import styles from './Technology.module.scss';
 import data from '../../utils/data.json';
 import useDeviceSize from '../../hooks/useDeviceSize';
+import React from 'react';
 export default function Technology () {
     const { id } = useParams();
     const navigate = useNavigate();
     const {isDesktop} = useDeviceSize();
+    const [transitionClass, setTransitionClass] = React.useState('');
 
     const techonologies = data.technology;
     const technology = techonologies.find(i => i.name.toLowerCase().replace(" ", "-") == (id as string)) ?? techonologies[0];
 
     const handleSelectTechnology = (technology: any) => {
         if (technology.name !== id) {
-            navigate(`/technology/${technology.name.toLowerCase().replace(" ", "-")}`);     
+            setTransitionClass(styles['fade-out']); // Add fade-out class
+            setTimeout(() => {
+                navigate(`/technology/${technology.name.toLowerCase().replace(" ", "-")}`);     
+                setTransitionClass(styles['fade-in']); 
+            }, 150); 
         }
     }
+
+    React.useEffect(() => {
+        const timeout = setTimeout(() => setTransitionClass(''), 150);
+        return () => clearTimeout(timeout);
+    }, [technology]);
 
     return (
         <div className={styles.container}>
@@ -23,7 +34,7 @@ export default function Technology () {
                 <span className='text-6'>SPACE LAUNCH 101</span>
             </div>
             <div className={styles.technology__container}>
-                <img className={styles.technology__img} src={isDesktop ? technology.images.portrait : technology.images.landscape} alt={`image of ${technology.name}`} />
+                <img className={`${styles.technology__img} ${transitionClass}`} src={isDesktop ? technology.images.portrait : technology.images.landscape} alt={`image of ${technology.name}`} />
                 <div className={styles.technology__details}>
                     <div className={styles.technology__tabs}>
                         {techonologies.map((item, index) => {
@@ -36,7 +47,7 @@ export default function Technology () {
                             )
                         })}
                     </div>
-                    <div className={styles.technology__info}>
+                    <div className={`${styles.technology__info} ${transitionClass}`}>
                         <div className={styles.technology__title}>
                             <div className={`text-4 ${isDesktop ? "" : "text-center"} text-half-white`}>THE TERMINOLOGY</div>
                             <div className={`text-3 ${isDesktop ? "" : "text-center"}`}>{technology.name.toUpperCase()}</div>
