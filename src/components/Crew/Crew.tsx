aa8ca9eb90a15e7239d676ee7e8993dc88132fc8
Sophia See <sophiasee11@gmail.com>
2025-01-01 19:23:32 +0800

    fixed crew desktop

--- src/components/Crew/Crew.tsx
+++ src/components/Crew/Crew.tsx
@@ -1,10 +1,12 @@
 import styles from './Crew.module.scss';
 import data from '../../utils/data.json';
 import { useNavigate, useParams } from 'react-router-dom';
+import useDeviceSize from '../../hooks/useDeviceSize';
 
 export default function Crew () {
     const { id } = useParams();
     const navigate = useNavigate();
+    const { isDesktop } = useDeviceSize();
     
     const crews = data.crew;
     const crewIndex = crews.findIndex(i => i.name.toLowerCase().replace(" ", "-") == (id as string)) ?? crews[0];
@@ -18,34 +20,37 @@ export default function Crew () {
 
     return (
         <div className={styles.container}>
-            <div className={styles.crew__texts}>
-                <div className='step__title'>
-                    <span className='step__number'>02</span>
-                    <span className='text-6'>MEET YOUR CREW</span>
-                </div>
-                <div className={styles.crew__details}>
-                    <div className={styles.crew__info}>
-                        <div className={styles.crew__title}>
-                            <div className="text-4 text-center text-half-white">{crew.role.toUpperCase()}</div>
-                            <div className="text-3 text-center">{crew.name.toUpperCase()}</div>                            
+            <div className='step__title'>
+                <span className='step__number'>02</span>
+                <span className='text-6'>MEET YOUR CREW</span>
+            </div>
+            <div className={styles.crew__container}>
+                <div className={styles.crew__texts}>    
+                    <div className={styles.crew__details}>
+                        <div className={styles.crew__info}>
+                            <div className={styles.crew__title}>
+                                <div className={`text-4 ${isDesktop ? "" : "text-center"} text-half-white`}>{crew.role.toUpperCase()}</div>
+                                <div className={`text-3 ${isDesktop ? "" : "text-center"}`}>{crew.name.toUpperCase()}</div>                            
+                            </div>
+                            <div className={`${styles.crew__bio} text-9 ${isDesktop ? "" : "text-center"} text-blue-300`}>{crew.bio}</div>
                         </div>
-                        <div className={`${styles.crew__bio} text-9 text-center text-blue-300`}>{crew.bio}</div>
+                    </div>
+                    <div className={styles.crew__pages}>
+                        {crews.map((item) => {
+                            const isActive = item.name === crew.name;
+
+                            return (
+                                <div className={`${styles.crew__page_item} ${isActive ? styles.active : ''}`} onClick={() => handleSelectCrew(item)}>
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
-                <div className={styles.crew__pages}>
-                    {crews.map((item) => {
-                        const isActive = item.name === crew.name;
-
-                        return (
-                            <div className={`${styles.crew__page_item} ${isActive ? styles.active : ''}`} onClick={() => handleSelectCrew(item)}>
-                            </div>
-                        )
-                    })}
+                <div className={styles.img_container}>
+                    <img className={styles.crew__image} src={crew.images.webp} alt={`image of ${crew.name}`} />
                 </div>
             </div>
-            <div className={styles.img_container}>
-                <img className={styles.crew__image} src={crew.images.webp} alt={`image of ${crew.name}`} />
-            </div>
+
         </div>
     )
 }
